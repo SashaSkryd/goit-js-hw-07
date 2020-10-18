7ceb17426f9965a774a72e7dae66e7d515392ef1
Sasha Skryd <sashaskryd@gmail.com>
2020-10-19 00:15:03 +0300

    2 ex done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Задание 1
 // Напиши скрипт, который выполнит следующие операции.
 
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
@@ -23,5 +24,56 @@ console.log([...refs.list.children]);// почему без консоли не 
     console.log(el.textContent)
 })
 
+// =================================================================
+// Задание 2
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
 
+// const ingredients = [
+//   'Картошка',
+//   'Грибы',
+//   'Чеснок',
+//   'Помидоры',
+//   'Зелень',
+//   'Приправы',
+// ];
 
+// const refs = {
+//     root: document.querySelector('body'),
+//     list: document.createElement('ul'),
+
+// }
+
+// ingredients.map(text => {
+//     let item = document.createElement('li')
+//     item.textContent = text
+//     refs.list.append(item)
+// })
+// =================================================================
+// Задание 3
+// Напиши скрипт для создания галлереи изображений по массиву данных.
+
+// В HTML есть список ul#gallery.
+
+// <ul id="gallery"></ul>
+// Используй массив объектов images для создания тегов img вложенных в li. Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+
+// const images = [
+//   {
+//     url:
+//       'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'White and Black Long Fur Cat',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Group of Horses Running',
+//   },
+// ];
